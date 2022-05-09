cb5acbdffebe6f42a557643118286e79ea6fe4bb
Duongdtrai <phamtungduong06032002@gmail.com>
2022-05-09 11:11:00 +0700

    deptrai

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	cout << "Duong da chinh sua lan 3";
+int main()
+{
+	cout << "Duong da chinh sua lan 4";
 	// ok nhe
 	return 0;
 }
